a9024d2a75106adcf61dc98f856ca80c8c4cbfbf
Rekicho <up201604145@fe.up.pt>
2018-10-01 17:01:20 +0100

    Background

--- LAIGDemoParser20182019v2/XMLscene.js
+++ LAIGDemoParser20182019v2/XMLscene.js
@@ -83,9 +83,8 @@ class XMLscene extends CGFscene {
     onGraphLoaded() {
         this.axis = new CGFaxis(this, this.graph.axisLength);
 
-        console.log(this.graph.ambient);
-
         this.setGlobalAmbientLight(this.graph.ambient[0],this.graph.ambient[1],this.graph.ambient[2],this.graph.ambient[3]);
+        this.gl.clearColor(this.graph.background[0],this.graph.background[1],this.graph.background[2],this.graph.background[3]);
 
         this.initLights();
 
