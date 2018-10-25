a2ee364b88d1cabe616d0ca2b55cbbbcce25f259
Rekicho <up201604145@fe.up.pt>
2018-10-25 18:55:27 +0100

    Fixed linear animation

--- LAIGDemoParser20182019v2/LinearAnimation.js
+++ LAIGDemoParser20182019v2/LinearAnimation.js
@@ -12,12 +12,12 @@ class LinearAnimation extends Animation {
         this.timePassed %= this.time;
 
         var pos = Math.floor(this.timePassed / this.timePerPoint);
-        var timeInPoint = this.timePassed % this.timePerPoint;
+        var timeInPoint = (this.timePassed % this.timePerPoint) / this.timePerPoint;
 
         var x = (this.controlPoints[pos + 1][0] -  this.controlPoints[pos][0]) * timeInPoint + this.controlPoints[pos][0];
         var y = (this.controlPoints[pos + 1][1] -  this.controlPoints[pos][1]) * timeInPoint + this.controlPoints[pos][1];
         var z = (this.controlPoints[pos + 1][2] -  this.controlPoints[pos][2]) * timeInPoint + this.controlPoints[pos][2];
-
+        
         this.scene.translate(x,y,z);
 
         //TODO: weird rotation shit
